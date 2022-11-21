b582ba5602b236edaf086bfe2dca9f7e190f53bc
Max Dumas <max@dumas.nyc>
2022-11-20 20:44:30 -0500

    Add more nuanced electricity pricing

--- esc/electricity_rates.py
+++ esc/electricity_rates.py
@@ -5,14 +5,10 @@ from typing import Any, Union
 import numpy as np
 from numpy.typing import ArrayLike, NDArray
 
-from .util import MINUTES_PER_DAY, minute_in_day
+from .util import minute_in_day
 
-EIGHT_AM_MINUTES = 8 * 60
-
-# Taken from https://www.coned.com/en/accounts-billing/your-bill/time-of-use
-off_peak_rates = np.ones(EIGHT_AM_MINUTES) * 0.018
-peak_rates = np.ones(MINUTES_PER_DAY - EIGHT_AM_MINUTES) * 0.255
-all_rates = np.concatenate((off_peak_rates, peak_rates), axis=None)
+# See electricity_rates.ipynb for how this was generated
+all_rates = np.load("../data/nyc_price_by_minute_of_day_2022-08-01.npy")
 
 def electricity_rate(t: ArrayLike) -> Union[np.floating[Any], NDArray[np.floating[Any]]]:
     return all_rates[minute_in_day(t)]
