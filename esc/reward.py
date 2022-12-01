fefa0c5072061ec4793be7e420b710575f73cecf
Jen L <64630443+jenny-ll@users.noreply.github.com>
2022-12-01 02:09:07 +0000

    Pending changes exported from your codespace

--- esc/reward.py
+++ esc/reward.py
@@ -2,12 +2,15 @@ from scipy.special import expit
 
 from esc.util import MEAN_WATER_TANK_HEIGHT_M
 
+
 def reward_high_tank_head(tank_head):
     """A function that returns 1 when the tank is full, and 0 when the tank is
     empty, with an S-shaped curve for all values between."""
     return expit(6 * (2 * tank_head / MEAN_WATER_TANK_HEIGHT_M - 1))
 
+
 def reward_low_energy_cost(energy_cost):
     """A function that returns 1 if energy_cost is 0, and returns reward
     asymptotically approaching 0 as energy cost increases."""
-    return 1.0 / (energy_cost + 1.0) ** 0.25
+    # return 1.0 / (energy_cost + 1.0) ** 0.25
+    return -(1/200) * energy_cost + 1
